80e5008c8a12b9f08258f2579434871d50a6e70b
jteng <akariass@gmail.com>
2018-12-04 16:42:40 -0500

    work

---
+++ nearest_replica.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import requests
+import argparse
+from math import sin, asin, cos, sqrt, atan2, radians
+
+parser = argparse.ArgumentParser(description='Replica locator for project 5.')
+parser.add_argument('ip', help='the ip to reference against')
+args = parser.parse_args()
+
+ip = args.ip
+
+replicas = [
+        '54.186.185.27',
+        '54.199.204.174',
+        '54.72.143.213',
+        '54.84.248.26']
+
+replica_locs = []
+
+for r in replicas:
+    res = requests.get('http://ip-api.com/json/' + r).json()
+    replica_locs.append((res['lat'], res['lon']))
+
+res = requests.get('http://ip-api.com/json/' + ip).json()
+iploc = (res['lat'], res['lon'])
+
+def distance(c1, c2):
+    lat1 = c1[0]
+    lon1 = c1[1]
+    lat2 = c2[0]
+    lon2 = c2[1]
+    p = 0.017453292519943295     #Pi/180
+    a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    return 12742 * asin(sqrt(a))
+
+closest = replicas[0]
+d = distance(iploc, replica_locs[0])
+for i in range(len(replicas)):
+    new_d = distance(iploc, replica_locs[i])
+    if new_d  < d:
+        closest = replicas[i]
+        d = new_d
+
+print(closest)
